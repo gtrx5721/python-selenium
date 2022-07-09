e2d9a41085eec7a40269632b35456f8e22da332b
gtrx5721 <accord89@inbox.ru>
2022-07-09 17:13:11 +0300

    add locators

--- test_main_page.py
+++ test_main_page.py
@@ -5,14 +5,17 @@ from selenium.webdriver.chrome.options import Options
 from pages.main_page import MainPage
 
 
+link1 = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+link2 = "http://selenium1py.pythonanywhere.com/"
+
+link = link2
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
